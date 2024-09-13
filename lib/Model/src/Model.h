e530b4fc6f1c18e9361f0ee4099a489494bfd179
Nick Jugganaikloo <nick.jgniklu@gmail.com>
2024-09-13 12:33:01 -0600

    test: autogen tests for esp32 model

---
+++ lib/Model/src/Model.h
@@ -0,0 +1,14 @@
+
+// automatically generated file from training\esp32_transfer_learning.ipynb
+#include <tensorflow/lite/core/c/common.h>
+#include <sys/types.h>
+#ifndef MODEL
+#define MODEL
+
+extern const unsigned char __model_tflite[];
+extern unsigned int __model_tflite_len;
+extern float model_quantization_scale;
+extern int model_quantization_zero_point;
+extern float default_model_prediction_threshold;
+
+#endif
